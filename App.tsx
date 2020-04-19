574811382653332e8499e3181e7a204afdba96fb
Rodolphe Lemasquerier <rodolphe.lemasquerier@gmail.com>
2020-04-19 21:28:43 +0200

    feat(iOS): Install dp3t-sdk-react-native

--- App.tsx
+++ App.tsx
@@ -1,7 +1,18 @@
 import React from 'react';
-import {StyleSheet, View, Text} from 'react-native';
+import {StyleSheet, View, Text, NativeModules} from 'react-native';
+import RNDP3TSDK from 'dp3t-sdk-react-native';
 
 const App = () => {
+  console.warn(NativeModules.RNDP3TSDK);
+
+  RNDP3TSDK.initialize('rntest', 'dev')
+    .then((res) => {
+      console.warn('res', res);
+    })
+    .catch((error) => {
+      console.warn(error);
+    });
+
   return (
     <View style={styles.container}>
       <Text>Hello!</Text>
